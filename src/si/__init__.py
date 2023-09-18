9b2401afc343d3fe42b4f4f991bacbc0a2f08e02
Bruno Sá <81045291+BrunoAGSa@users.noreply.github.com>
2023-09-18 12:04:46 +0100

    Update __init__.py
    Adding my co-authorship to README.md file

--- src/si/__init__.py
+++ src/si/__init__.py
@@ -1,9 +1,9 @@
-__author__ = "João Correia"
-__credits__ = ["João Correia, Fernando Cruz", "Vítor Pereira"]
+__author__ = "Bruno Sá"
+__credits__ = ["Bruno Sá"]
 __license__ = "Apache License 2.0"
 __version__ = "0.0.1"
-__maintainer__ = "João Correia"
-__email__ = "jfscorreia95@gmail.com"
+__maintainer__ = "Bruno Sá"
+__email__ = "sa.bruno.2001@gmail.com"
 
 # -----------------------------------------------------------------------------------
 # This package is heavily inspired and adapted from
